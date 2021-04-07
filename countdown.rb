7fe8922e218d96672a6f53027424ec2598c74d75
Heshie Brody <4hbrody@gmail.com>
2021-04-07 22:43:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,1 @@
 #write your code here
-
-def countdown(number)
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
-  end
-  #Countdown Done
-  return "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(number)
-  sleep(number)
-end
